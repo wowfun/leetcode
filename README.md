e03eb5abc9918c1282a999c6ac4eb92d4abf925e
Sinputer <haggi.cho@outlook.com>
2019-05-28 18:20:22 +0800

    add solution for problem 1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # leetcode
 
-[LeetCode Algorithm](https://github.com/wowfun/leetcode/tree/master/algorithm)
+## [LeetCode Algorithms](./algorithms)
+
+| # | Title | Solution | Difficulty |
+| --- | --- | --- | --- |
+| 1 | [Two Sum](https://leetcode.com/problems/two-sum) | [Java](./algorithms/java/src/twosum/Solution.java) | Easy
---
+++ algorithms/java/src/twosum/Solution.java
@@ -0,0 +1,17 @@
+package twosum;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] s;
+        for (int i=0;i<nums.length;i++){
+            int b=target-nums[i];
+            for (int j=i+1;j<nums.length;j++){
+                if(nums[j]==b){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
