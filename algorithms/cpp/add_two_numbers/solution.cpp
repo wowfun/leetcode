59dfdab26a6d86e6e8491f00c1c9f8ccbaa91d3f
Sinputer <haggi.cho@outlook.com>
2019-06-03 19:32:30 +0800

    update solution for p2

--- algorithms/cpp/add_two_numbers/solution.cpp
+++ algorithms/cpp/add_two_numbers/solution.cpp
@@ -7,15 +7,6 @@
  * };
  */
 
-#include <iostream>
-using namespace std;
-
-struct ListNode {
-  int val;
-  ListNode* next;
-  ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
  public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -42,45 +33,3 @@ class Solution {
     return l3;
   }
 };
-
-// test
-
-ListNode* createLinkListNum(int num) {
-  int head = -1;
-  ListNode* l=new ListNode(head);
-  l->next = NULL;
-  while (num > 0) {
-    ListNode* p;
-    p = new ListNode(num % 10);
-    p->next = l->next;
-    l->next = p;
-    num = num / 10;
-  }
-  return l;
-}
-
-void printLinkList(ListNode* l) {
-  ListNode* p;
-  p = l->next;
-  while (p) {
-    cout<<p->val;
-    p = p->next;
-  }
-  cout << endl;
-}
-
-void test() {
-  ListNode* l1 = createLinkListNum(123);
-  ListNode* l2 = createLinkListNum(5678);
-  ListNode* l3;
-  Solution* s1 = new Solution();
-  l3 = s1->addTwoNumbers(l1, l2);
-  printLinkList(l1);
-  printLinkList(l2);
-  printLinkList(l3);
-}
-
-int main() {
-  test();
-  return 0;
-}
