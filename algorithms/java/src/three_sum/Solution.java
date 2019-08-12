bccb6a4e21394aacc8aee9cabf4baea854948be1
wowfun <haggi.cho@outlook.com>
2019-08-12 17:02:10 +0800

    add s15

---
+++ algorithms/java/src/three_sum/Solution.java
@@ -0,0 +1,31 @@
+package three_sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> threeSumListList = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    threeSumListList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return threeSumListList;
+    }
+}
---
+++ algorithms/java/src/three_sum/Test.java
@@ -0,0 +1,9 @@
+package three_sum;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums1={-1, 0, 1, 2, -1, -4};
+        Solution s1 = new Solution();
+        System.out.println(s1.threeSum(nums1));
+    }
+}
